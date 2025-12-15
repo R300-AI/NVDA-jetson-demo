6614a0a847bebaef1ca57b966348936c92ce1bff
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2025-12-15 14:05:03 +0800

    Update vector_add.cu

--- practice2/vector_add.cu
+++ practice2/vector_add.cu
@@ -1,15 +1,16 @@
-#include <iostream>
 #include <cuda_runtime.h>
+#include <iostream>
 using namespace std;
 
 __global__ void vectorAdd(const float* A, const float* B, float* C, int N) {
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
-    if (i < N) C[i] = A[i] + B[i];
+    // TODO: 計算 thread 的全域索引 i
+    // TODO: 完成 C[i] = A[i] + B[i]
 }
 
 int main() {
-    int N = 1<<20;
+    const int N = 1<<20;
     size_t size = N * sizeof(float);
+
     float *h_A = new float[N], *h_B = new float[N], *h_C = new float[N];
     for(int i=0;i<N;i++){ h_A[i]=1.0f; h_B[i]=2.0f; }
 
@@ -18,9 +19,8 @@ int main() {
     cudaMemcpy(d_A,h_A,size,cudaMemcpyHostToDevice);
     cudaMemcpy(d_B,h_B,size,cudaMemcpyHostToDevice);
 
-    int threadsPerBlock=256;
-    int blocksPerGrid=(N+threadsPerBlock-1)/threadsPerBlock;
-    vectorAdd<<<blocksPerGrid,threadsPerBlock>>>(d_A,d_B,d_C,N);
+    // TODO: 設定 threadsPerBlock 與 blocksPerGrid
+    // TODO: 呼叫 kernel
 
     cudaMemcpy(h_C,d_C,size,cudaMemcpyDeviceToHost);
     cout<<"C[0]="<<h_C[0]<<endl;

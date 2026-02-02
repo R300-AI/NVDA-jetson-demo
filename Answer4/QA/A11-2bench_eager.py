790e134771e329aa6486c0c2cb6686c5a76b42bc
Sylvia <164860092+SylviaChanITRI@users.noreply.github.com>
2026-02-02 19:43:34 +0800

    Create A11-2bench_eager.py

---
+++ Answer4/QA/A11-2bench_eager.py
@@ -0,0 +1,11 @@
+## colab
+# bench_eager.py
+import torch
+
+def main_eager():
+    device = "cuda" if torch.cuda._____( ) else "cpu"   # === TODO: is_available
+    print("Device:", device, "| PyTorch:", torch.__version__)
+
+    model = SmallCNN()
+    fps = bench_infer(model, device=device, iters=200, batch=64)
+    print(f"[PyTorch Eager] {fps:10.2f} samples/sec")
